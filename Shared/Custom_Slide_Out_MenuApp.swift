3865f6b4673c560c744c1adeea83dd237ab7d889
yuan <dnstylish@gmail.com>
2021-10-21 19:13:35 +0700

    Initial Commit

---
+++ Shared/Custom_Slide_Out_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Slide_Out_MenuApp.swift
+//  Shared
+//
+//  Created by Yuan on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Slide_Out_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
